5c0b7dd76a31b9f5c7aeaab9b7f699cab9fe9d38
noobweer <mokrowon@gmail.com>
2025-06-10 16:21:53 +1000

    💻 Delete, edit transactions views

--- manager/views.py
+++ manager/views.py
@@ -9,5 +9,25 @@ from .services.transactions_services import *
 @permission_classes([IsAuthenticated])
 class CreateTransactions(APIView):
     def post(self, request):
-        create_result = TransactionsService().create_transaction(request.data)
+        data = request.data
+
+        create_result = TransactionsService().create_transaction(data)
         return Response(create_result)
+
+
+@permission_classes([IsAuthenticated])
+class DeleteTransaction(APIView):
+    def post(self, request):
+        data = request.data
+
+        delete_result = TransactionsService().delete_transaction(data)
+        return Response(delete_result)
+
+
+@permission_classes([IsAuthenticated])
+class EditTransaction(APIView):
+    def post(self, request):
+        data = request.data
+
+        edit_result = TransactionsService().edit_transaction(data)
+        return Response(edit_result)
